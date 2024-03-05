ab0d8978ac27c426d6038ed2ecad2c337e571382
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-03-05 04:00:31 +0000

    started on the js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import '../style/index.scss';
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+window.onload = () => {
+  document.querySelector('.card').classList.add('heart');
+  setTimeout(() => {
+    document.querySelector('.card').classList.remove('heart');
+    document.querySelector('.card').classList.add('spade');
+  }, 1000)
 };
